42b198c543e865e24685ac1e9ba7d63223faf234
me <me@gmail.com>
2021-07-10 13:46:23 +0100

    Email verification

---
+++ resources/views/mailVerification.blade.php
@@ -0,0 +1,71 @@
+<html><head></head><body> 
+<div style="box-sizing:border-box;display:block;max-width:600px;margin:0 auto;padding:10px"> 
+<div style="box-sizing:border-box;width:100%;margin-bottom:30px;margin-top:15px"> 
+<table style="box-sizing:border-box;width:100%;border-spacing:0;border-collapse:separate!important" width="100%"> 
+<tbody> 
+<tr> 
+<td style="box-sizing:border-box;padding:0;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top;text-align:left" valign="top" align="left"><a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55SzLmhKGS6lriqkAxiPdALchgxVePDyghb7MFJHAYY5U-Q28uQS_qXN8UFtFwAmFb1ys=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55SzLmhKGS6lriqkAxiPdALchgxVePDyghb7MFJHAYY5U-Q28uQS_qXN8UFtFwAmFb1ys%3D&amp;source=gmail&amp;ust=1625989035290000&amp;usg=AFQjCNEOszifgHFxlOKSBE4hjFg7N_Z9og"><img src="./Asset 4300x.png" height="42" alt="SendGrid" style="max-width:100%;border-style:none" class="CToWUd"></a></td> 
+</tr> 
+</tbody> 
+</table> 
+</div> 
+<div style="box-sizing:border-box;width:100%;margin-bottom:30px;background:#ffffff;border:1px solid #f0f0f0"> 
+<table style="box-sizing:border-box;width:100%;border-spacing:0;border-collapse:separate!important" width="100%"> 
+<tbody> 
+<tr> 
+<td style="box-sizing:border-box;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top;padding:30px" valign="top"> 
+<table style="box-sizing:border-box;width:100%;border-spacing:0;border-collapse:separate!important" width="100%"> 
+<tbody> 
+<tr> 
+<td style="box-sizing:border-box;padding:0;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top" valign="top"> 
+<div id="m_852281162277957744block-one" style="box-sizing:border-box">
+<h2 style="margin:0;margin-bottom:30px;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;line-height:1.5;font-size:24px;color:#294661!important">Hi auto,</h2> 
+<p style="margin:0;margin-bottom:30px;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;font-weight:300">Thanks for choosing&nbsp;Cryptobolt platform as your personal investment service!&nbsp;<!-- <a href="https://sendgrid.com/email-template-download/?post=48575" title="Click this link" style="color:#348eda;font-weight:400;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://sendgrid.com/email-template-download/?post%3D48575&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNFyecrcs9a6P_kNDXax8tyTdV8Njg">Click this link</a> --><br><br>
+Please verify your email address to get access to all our live investment opportunities, as well as be able to start raising investment..<!--  to access the HTML code and copy it into  --><a style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none" href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55TMuzJzySREpOsGj797iOXDKUolI1BPwAd9BL4FwkKZNRz1ZJkHmmnD8SC8ldBi8U4Mk=" id="m_852281162277957744" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55TMuzJzySREpOsGj797iOXDKUolI1BPwAd9BL4FwkKZNRz1ZJkHmmnD8SC8ldBi8U4Mk%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNGA7BkLPv1nfnB268Xh6wdMMD4ZxA"></a> .</p> 
+<p style="margin:0;margin-bottom:30px;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;font-weight:300">Happy Sending,<br>Cryptobolt © Support Team.</p>
+</div> </td> 
+</tr> 
+<tr> 
+<td style="box-sizing:border-box;padding:0;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top" valign="top"> 
+<table cellpadding="0" cellspacing="0" style="box-sizing:border-box;border-spacing:0;width:100%;border-collapse:separate!important" width="100%"> 
+<tbody> 
+<tr> 
+<td align="center" style="box-sizing:border-box;padding:0;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top;padding-bottom:15px" valign="top"> 
+<table cellpadding="0" cellspacing="0" style="box-sizing:border-box;border-spacing:0;width:auto;border-collapse:separate!important"> 
+<tbody> 
+<tr> 
+<td style="box-sizing:border-box;padding:0;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top;background-color:#348eda;border-radius:2px;text-align:center" valign="top" bgcolor="#348EDA" align="center"> 
+<div id="m_852281162277957744primary-button" style="box-sizing:border-box">
+<div> 
+<a href="http://links.twilio.com/dc/Kze1Sx8UAIQmEZdMZ0I3R6UaeTAWS3F3_ZfLhOOqdzkIveMx0yfn9l6cX8aMYVJBFRO82o89h2WSYyhoI6fsYA==/Mjk0LVRLQi0zMDAAAAF-Lk55S4kW12cLzms1TFW6XbQsu-VWyCUTu5Ig9d82LqoLNd6j3GuXkhKhirmQwLywzbGGOGI=" style="box-sizing:border-box;border-color:#348eda;font-weight:400;text-decoration:none;display:inline-block;margin:0;padding:12px 45px;color:#ffffff;border:solid 1px #348eda;border-radius:2px;font-size:14px;text-transform:capitalize;background-color:#000" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/dc/Kze1Sx8UAIQmEZdMZ0I3R6UaeTAWS3F3_ZfLhOOqdzkIveMx0yfn9l6cX8aMYVJBFRO82o89h2WSYyhoI6fsYA%3D%3D/Mjk0LVRLQi0zMDAAAAF-Lk55S4kW12cLzms1TFW6XbQsu-VWyCUTu5Ig9d82LqoLNd6j3GuXkhKhirmQwLywzbGGOGI%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNEsN1C047fY8i4s73r2VFPLdWx3NA">Verify Email Now</a> 
+</div>
+</div> </td> 
+</tr> 
+</tbody> 
+</table> </td> 
+</tr> 
+</tbody> 
+</table> </td> 
+</tr> 
+<tr> 
+<td style="box-sizing:border-box;padding:0;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:16px;vertical-align:top" valign="top"> 
+<div id="m_852281162277957744block-three" style="box-sizing:border-box">
+<!-- <p style="margin:30px;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:14px;font-weight:300"><em>Ready to put Automation and Signup Forms to the test? Try our <a style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none" href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S-S_ouC4ihAODaZ1J3P31QOn_1Z1TVov9pXokA1ZyEpTK-c6iKELZqdGWTO64kcVQEk=" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S-S_ouC4ihAODaZ1J3P31QOn_1Z1TVov9pXokA1ZyEpTK-c6iKELZqdGWTO64kcVQEk%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNHP69A0nzMTZUYtSYxS4sG__7U3Cg">Free Marketing Campaigns plan</a> and send up to 6,000 emails/month.</em></p> -->
+</div> </td> 
+</tr> 
+</tbody> 
+</table> </td> 
+</tr> 
+</tbody> 
+</table> 
+</div> 
+<div style="box-sizing:border-box;clear:both;width:100%"> 
+<table style="box-sizing:border-box;width:100%;border-spacing:0;font-size:12px;border-collapse:separate!important" width="100%"> 
+<tbody> 
+<tr style="font-size:12px"> 
+<td style="box-sizing:border-box;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;vertical-align:top;font-size:12px;text-align:center;padding:20px 0" valign="top" align="center"><a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55SzLmhKGS6lriqkAxiPdALchgxVePDyghb7MFJHAYY5U-Q28uQS_qXN8UFtFwAmFb1ys=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none;font-size:12px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55SzLmhKGS6lriqkAxiPdALchgxVePDyghb7MFJHAYY5U-Q28uQS_qXN8UFtFwAmFb1ys%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNGn8IvG1s2_Jm4pqK6RupuLrBVsJw"><img src="{{asset('img/logo2.png')}}" height="36" alt="SendGrid" style="max-width:100%;border-style:none;font-size:12px" class="CToWUd"></a> <p style="color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;margin-bottom:5px;margin:10px 0 20px">Send with Confidence</p> <p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px">© SendGrid Inc. 1801 California St. Suite 500, Denver, CO 80202 USA</p> <p style="margin:0;color:#294661;font-family:'Open Sans','Helvetica Neue','Helvetica',Helvetica,Arial,sans-serif;font-weight:300;font-size:12px;margin-bottom:5px"><a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S3mkn0KLgVQjQISJTyamMg-vF6hER2bWelpQJWE9mtWdrT4PJ0SBAAtb0eJOypQaXXI=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none;font-size:12px;padding:0 5px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S3mkn0KLgVQjQISJTyamMg-vF6hER2bWelpQJWE9mtWdrT4PJ0SBAAtb0eJOypQaXXI%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNFDiP_-DeRZQWMq5LNx9c4Ce-VeIw">Blog</a> <a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55TDrJRT6HiC0CFdALBaDpebLC1e_LKDDsVBZWQqJudefWIe_Oo4-VZTKekb8M1O8CcmE=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none;font-size:12px;padding:0 5px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55TDrJRT6HiC0CFdALBaDpebLC1e_LKDDsVBZWQqJudefWIe_Oo4-VZTKekb8M1O8CcmE%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNEs8C_vNfuIZ03RAlGjB-12hJ7nQQ">GitHub</a> <a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S6E3O4DSiUfFrdiBF_rzeXZkxjtsiQ6VmybGX_QdcW9YrgKOSQX_w1uxj_EBldcjsSY=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none;font-size:12px;padding:0 5px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S6E3O4DSiUfFrdiBF_rzeXZkxjtsiQ6VmybGX_QdcW9YrgKOSQX_w1uxj_EBldcjsSY%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNFF-T80oN6gxkUfU4b5LculqFr7Iw">Twitter</a> <a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S5lYOb32wvEcVUhBY4MqAU_3RtxS7rWaWanYJyWn6ZWNoqk32m-A_FgjLCM8m6TV1iA=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none;font-size:12px;padding:0 5px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S5lYOb32wvEcVUhBY4MqAU_3RtxS7rWaWanYJyWn6ZWNoqk32m-A_FgjLCM8m6TV1iA%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNERP5IvpaGlV0AyYIbi4vtxBWJRjA">Facebook</a> <a href="http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S320IVBrFo7xjUKQpM03Bhiwn6lNdGrQO2CdVEZdb_saZkPZKGVW6ttybumITTs4kq4=" style="box-sizing:border-box;color:#348eda;font-weight:400;text-decoration:none;font-size:12px;padding:0 5px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://links.twilio.com/Mjk0LVRLQi0zMDAAAAF-Lk55S320IVBrFo7xjUKQpM03Bhiwn6lNdGrQO2CdVEZdb_saZkPZKGVW6ttybumITTs4kq4%3D&amp;source=gmail&amp;ust=1625989035291000&amp;usg=AFQjCNHO0xW8G0FTEYJvg0D9RYGNWWHivg">LinkedIn</a></p> </td> 
+</tr> 
+</tbody> 
+</table> 
+</div> 
+</div> </body></html>
