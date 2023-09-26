58a61ababef9b26629774fe59898c3a688fadb16
IDOWU OLUWATOBI DAMILOLA <82226019+logs94@users.noreply.github.com>
2023-09-26 07:58:44 +0100

    price tag desktop sub nav on almost resolve

--- src/Modules/Site/Resources/views/index.blade.php
+++ src/Modules/Site/Resources/views/index.blade.php
@@ -57,9 +57,9 @@
 
 
         /* ======= FLEX ===== */
-        .flex-container {
-            /* display: -webkit-box;
-            display: -ms-flexbox; */
+        .flex-container .price-box {
+            display: -webkit-box;
+            display: -ms-flexbox;
             display: flex;
             /* -ms-flex-pack: distribute; */
             justify-content: space-around;
@@ -67,9 +67,9 @@
             flex-wrap: wrap; */
         }
 
-    .box {
-        width: 98%;
-        border: 1px solid blue;
+    .price-box {
+        width: 98%!important;
+        /* border: 1px solid blue; */
         padding: 5px;
         background-color: transparent;
         margin-top: 5px;
@@ -101,7 +101,7 @@
 
 
 @media only screen and (min-width: 576px) {
-  .box {
+  .price-box {
     background-color: transparent;
     width: 100%;
     margin-top: 5px;
@@ -114,22 +114,22 @@
     /* flex-wrap: wrap; */
     display: flex;
   }
-  .box {
+  .price-box {
     background-color: transparent;
-    width: 45%;
+    width: 100%;
     margin-top: 5px;
   }
 }
 
 @media only screen and (min-width: 992px) {
-  .box {
+  .price-box {
     background-color: green;
     width: 23%;
   }
 }
 
 @media only screen and (min-width: 1200px) {
-  .box {
+  .price-box {
     background-color: transparent!important;
   }
 }
@@ -306,7 +306,7 @@
                     <div class="nav__link dropdown__button"> Pricing <i class="ri-arrow-down-s-line dropdown__arrow"></i></div>
                     <div class="dropdown__container">
                         <div class="dropdown__content flex-container">
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group price-box">
                                 <div class="dropdown__icon"><img src="images/icon20.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">Starters Plan</span>
@@ -316,7 +316,7 @@
                                     <li><a href="#" class="dropdown__link">ROI: 187%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group price-box">
                                 <div class="dropdown__icon"><img src="images/icon21.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">Silver Plan</span>
@@ -326,7 +326,7 @@
                                     <li><a href="#" class="dropdown__link">ROI: 216%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group price-box">
                                 <div class="dropdown__icon"><img src="images/coin26.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">Gold Plan</span>
@@ -336,7 +336,7 @@
                                     <li><a href="#" class="dropdown__link">ROI: 245%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group price-box">
                                 <div class="dropdown__icon"><img src="images/icon22.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">Platinum olan</span>
@@ -346,7 +346,7 @@
                                     <li><a href="#" class="dropdown__link">ROI: 301%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group price-box">
                                 <div class="dropdown__icon"><img src="images/icoin23.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">NFT plan</span>
@@ -356,7 +356,7 @@
                                     <li><a href="#" class="dropdown__link">ROI: 301%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group  price-box">
                                 <div class="dropdown__icon"><img src="images/icon09.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">NPF Plan</span>
@@ -366,7 +366,7 @@
                                     <li><a href="#" class="dropdown__link">ROI: 301%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <div class="dropdown__group price-box">
                                 <div class="dropdown__icon"><img src="images/icon010.svg" alt="Icon" width="50"
                                                                  height="50"></div>
                                 <span class="dropdown__title">Quantum Plan</span>
@@ -376,15 +376,15 @@
                                     <li><a href="#" class="dropdown__link">ROI: 301%</a></li>
                                 </ul>
                             </div>
-                            <div class="dropdown__group item box">
+                            <!-- <div class="dropdown__group item price-box">
                                 <div class="dropdown__icon">
                                     <div class="next"><a href="{{route('register')}}"><img
                                                 src="images/Circle_arrow.svg" alt="Icon" width="50" height="50"
                                                 class="circlepoint"></a></div>
                                 </div>
                                 <span class="dropdown__title">see more</span>
-                                <!-- <ul class="dropdown__list"></ul> -->
-                            </div>
+                                <ul class="dropdown__list"></ul>
+                            </div> -->
                         </div>
                     </div>
                 </li>
